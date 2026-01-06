7709ae48123735a3479896105c698f00f0757b47
Santhosh <santhosh.v@wisework.in>
2026-01-06 22:05:46 +0530

    running

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,7 @@ import { lazy, Suspense } from "react";
 import MainLayout from "./components/Layout/Mainlayout/MainLayout";
 import { appRoutes } from "./routes/appRoutes";
 import { Spinner } from "./components/Common/Buttons";
-<<<<<<< HEAD
 import TierManagement from "./Pages/Course/Tier/TierManagement";
-=======
-import CourseContentsPage from "./Pages/Course/Course/CourseDetails";
->>>>>>> cd097b2b9d9b01b9f558c5b54a22fde399d807f1
 
 // 🔹 Lazy-loaded pages
 const SignInPage = lazy(() => import("./Pages/Auth/Auth"));
@@ -16,10 +12,6 @@ const CourseManagement = lazy(
   () => import("./Pages/Course/Course/CourseManagement")
 );
 
-<<<<<<< HEAD
-
-=======
->>>>>>> cd097b2b9d9b01b9f558c5b54a22fde399d807f1
 function App() {
   return (
     <Suspense fallback={<Spinner />}>
@@ -34,10 +26,7 @@ function App() {
 
           {/* Course Management */}
           <Route path={appRoutes.course.path} element={<CourseManagement />} />
-          <Route
-            path={appRoutes.course.children.courseDetails}
-            element={<CourseContentsPage />}
-          />
+         
 
           <Route 
             path={appRoutes.course.children.courseTiers}
--- src/Routes/appRoutes.ts
+++ src/Routes/appRoutes.ts
@@ -4,11 +4,8 @@ export const appRoutes = {
   course: {
     path: "/course",
     children: {
-<<<<<<< HEAD
       courseTiers: "/course/:id",
-=======
       courseDetails: "/course/:courseId",
->>>>>>> cd097b2b9d9b01b9f558c5b54a22fde399d807f1
     },
   },
 };
